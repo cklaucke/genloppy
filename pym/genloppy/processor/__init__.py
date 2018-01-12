40da906d5eae4a0166e3ae19486cace64921b638
cklaucke <33376323+cklaucke@users.noreply.github.com>
2018-01-12 18:37:30 +0100

    GH-9: implemented processor API and Base

---
+++ pym/genloppy/processor/__init__.py
@@ -0,0 +1,27 @@
+"""
+Provides the processor API
+
+realizes: R-PROCESSOR-API-001
+"""
+
+__author__ = "cklaucke"
+
+
+class Interface:
+    def callbacks(self):
+        """Returns a dict of modes and associated callbacks to be subscribed.
+
+        realizes: R-PROCESSOR-API-003"""
+        raise NotImplementedError
+
+    def pre_process(self):
+        """Does pre-processing before parsing has begun.
+
+        realizes: R-PROCESSOR-API-002"""
+        raise NotImplementedError
+
+    def post_process(self):
+        """Does post-processing after parsing has finished.
+
+        realizes: R-PROCESSOR-API-004"""
+        raise NotImplementedError
