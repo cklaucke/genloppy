ac0fea0250578be738f462b2c6ed4db6e226d975
cklaucke <33376323+cklaucke@users.noreply.github.com>
2018-02-09 19:55:26 +0100

    GH-0: refactored to have smaller functions

--- pym/genloppy/main.py
+++ pym/genloppy/main.py
@@ -19,29 +19,20 @@ class Main:
         self.configurator = configurator
         self.processor_factory = processor_factory
         self.elog_parser = elog_parser
+        self.processor = None
 
-    def run(self):
-        """
-        Obtains the desired processor, subscribes, pre-processes, invokes the parser and
-        post-processes.
-
-        realizes: R-MAIN-002
-        """
-        self.configurator.parse_arguments()
-        self._config_feature_check()
+    def create_processor(self):
         processor_configuration = dict(self.configurator.processor_configuration)
-        parser_configuration = dict(self.configurator.parser_configuration)
         processor_name = processor_configuration.pop("name")
-        processor = self.processor_factory.create(processor_name, **processor_configuration)
+        self.processor = self.processor_factory.create(processor_name, **processor_configuration)
+
+    def setup_parser(self):
+        parser_configuration = dict(self.configurator.parser_configuration)
         parser_configuration.pop("file_names")
         self.elog_parser.configure(**parser_configuration)
-        for mode, callback in processor.callbacks.items():
+        for mode, callback in self.processor.callbacks.items():
             self.elog_parser.subscribe(callback, mode)
 
-        processor.pre_process()
-        with open(DEFAULT_ELOG_FILE) as f:
-            self.elog_parser.parse(f)
-        processor.post_process()
 
     def _config_feature_check(self):
         """Checks for configuration feature availability."""
@@ -65,6 +56,23 @@ class Main:
         config_compare(self.configurator.parser_configuration, allowed_parser_configuration)
         config_compare(self.configurator.processor_configuration, allowed_processor_configuration)
         config_compare(self.configurator.output_configuration, allowed_output_configuration)
+
+    def run(self):
+        """
+        Obtains the desired processor, subscribes, pre-processes, invokes the parser and
+        post-processes.
+
+        realizes: R-MAIN-002
+        """
+        self.configurator.parse_arguments()
+        self._config_feature_check()
+        self.create_processor()
+        self.setup_parser()
+
+        self.processor.pre_process()
+        with open(DEFAULT_ELOG_FILE) as f:
+            self.elog_parser.parse(f)
+        self.processor.post_process()
 
 
 def main(argv):
