384eb394c5c778cc8ec8783170dd6e10a58515bb
cklaucke <ckl+dev@mailbox.org>
2019-03-16 23:42:07 +0100

    GH-41 implement default implementation for processor base output

--- pym/genloppy/processor/sync.py
+++ pym/genloppy/processor/sync.py
@@ -4,9 +4,8 @@ from genloppy.processor.base import BaseOutput
 class Sync(BaseOutput):
     """List sync processor implementation
     realizes: R-PROCESSOR-SYNC-001
+    realizes: R-PROCESSOR-SYNC-002
     """
-    HEADER = ""
-    TRAILER = ""
 
     def __init__(self, **kwargs):
         """Adds callback for 'sync'.
@@ -14,17 +13,7 @@ class Sync(BaseOutput):
         super().__init__(**kwargs)
         self._add_callbacks(sync=self.process)
 
-    def pre_process(self):
-        """Prints header.
-        realizes: R-PROCESSOR-SYNC-002"""
-        self.output.message(self.HEADER)
-
     def process(self, item):
         """Prints sync information.
-        realizes: R-PROCESSOR-SYNC-005"""
-        self.output.sync_item(item["timestamp"])
-
-    def post_process(self):
-        """Prints trailer.
         realizes: R-PROCESSOR-SYNC-004"""
-        self.output.message(self.TRAILER)
+        self.output.sync_item(item["timestamp"])
