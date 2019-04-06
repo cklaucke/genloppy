592763b6052c8691bd6849b421be58c733b4e77f
cklaucke <ckl+dev@mailbox.org>
2019-04-06 19:51:46 +0200

    GH-11 implement duration processor

---
+++ src/genloppy/processor/duration.py
@@ -0,0 +1,46 @@
+from genloppy.processor.base import Base
+
+
+class Duration(Base):
+    """Duration processor implementation
+    realizes: R-PROCESSOR-DURATION-001
+    """
+
+    def __init__(self, callback, **kwargs):
+        """Initializes the duration processor taking a callback
+        and optional keyword arguments.
+
+        :param callback: the callback to be called after a duration calculation
+
+        realizes: R-PROCESSOR-DURATION-002
+        realizes: R-PROCESSOR-DURATION-003
+        """
+        super().__init__(**kwargs)
+        self._add_callbacks(merge_begin=self.merge_begin,
+                            merge_end=self.merge_end)
+        self.callback = callback
+        self.current_merge = None
+
+    def merge_begin(self, properties):
+        """Saves the properties of the merge_begin entry
+
+        :param properties: the properties/token of the entry
+
+        realizes: R-PROCESSOR-DURATION-004
+        """
+        self.current_merge = properties
+
+    def merge_end(self, properties):
+        """Determines the merge duration of a matching merge_end entry and calls the callback
+
+        :param properties: the properties/token of the entry
+
+        realizes: R-PROCESSOR-DURATION-005
+        """
+        if self.current_merge:
+            keys = ["atom", "count_m", "count_n"]
+            if all(self.current_merge[key] == properties[key] for key in keys):
+                duration = int(properties["timestamp"]) - int(self.current_merge["timestamp"])
+                self.callback(properties, duration)
+
+        self.current_merge = None
